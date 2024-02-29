c562880bab054fa9d7c32fd9e118ed0eb8a39f3b
Odong Martin <odongmartin21@gmail.com>
2024-02-29 07:52:57 +0300

    commit

--- app/api/malaria/route.js
+++ app/api/malaria/route.js
@@ -31,10 +31,21 @@ export const GET = async (req) => {
     try {
         await connectToDB();
         const MalariaData = await MalariaCase.find({})
+        //aggregation to count positive cases per day
+        const positiveCasesPerDay = MalariaData.reduce((acc, curr) => {
+            const day = curr.date.toDateString(); // Get the date string
+            const existingDay = acc.find(entry => entry.day === day);
+            if (existingDay) {
+                existingDay.sales += 1; //increment the count for the current day
+            } else {
+                acc.push({ day, sales: 1 }); //add a new entry for the current day
+            }
+            return acc;
+        }, []);
 
-        //console.log("malaria Data:" + MalariaData);
-
-        return new Response(JSON.stringify(MalariaData), {status: 201});
+        console.log(JSON.stringify(positiveCasesPerDay))
+        //return the array of objects containing positive cases per day
+        return new Response(JSON.stringify(positiveCasesPerDay), { status: 201 });
 
     } catch (error) {
         return new Response("Failed to create new case", {status:500});
